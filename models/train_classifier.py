06973fc48f1d383be37f0eceb1f0d89371a6b8cb
Yuan Wang <yxw017@cs.bham.ac.uk>
2022-08-21 23:18:21 +0100

    Finished up the model and web app

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,71 @@
 import sys
-
+import numpy as np
+import pandas as pd
+from sqlalchemy import create_engine
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+import re
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.model_selection import GridSearchCV
+import pickle
+nltk.download(['wordnet', 'punkt', 'stopwords'])
 
 def load_data(database_filepath):
-    pass
+    engine = create_engine(f"sqlite:///{database_filepath}")
+    df = pd.read_sql_table('disaster_response',engine)
+    X = df["message"] # message column fro training
+    Y = df.iloc[:, 4:] # 36 categories for target
+    category_names = Y.columns
+    return X, Y, category_names
 
 
 def tokenize(text):
-    pass
+    # text normalize
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower())
+    # tokenize
+    words = word_tokenize(text)
+    # stop words removal
+    stopword = [w for w in words if w not in stopwords.words("english")]
+    # Lemmatization process
+    clean_tokens =[WordNetLemmatizer().lemmatize(w) for w in stopword]
+    return clean_tokens
+
 
 
 def build_model():
-    pass
+    pipeline = Pipeline([
+        ('vect', CountVectorizer()),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(RandomForestClassifier()))])
+    
+    parameters = {
+        #'tfidf__use_idf': (True, False),
+        'clf__estimator__n_estimators': [50, 100]
+        #'clf__estimator__min_samples_split': [2, 4]
+        } 
+    cv = GridSearchCV(pipeline, param_grid=parameters)    
+    return cv
+
+    
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    y_pred = model.predict(X_test)
+    print(classification_report(y_pred, Y_test.values, target_names=category_names))
+    # print raw accuracy score 
+    print('Accuracy Score: {}'.format(np.mean(Y_test.values == y_pred)))
 
 
 def save_model(model, model_filepath):
-    pass
+     with open(model_filepath, 'wb') as f:
+        pickle.dump(model, f)
 
 
 def main():
