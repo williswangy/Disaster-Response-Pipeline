033070569d96a0c5fe299f097937608baff88d8e
Yuan Wang <yxw017@cs.bham.ac.uk>
2022-08-22 22:08:33 +0100

    fixed the ValueError: not enough values to unpack by removing the extra arugument

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -77,7 +77,7 @@ def main():
     if len(sys.argv) == 3:
         database_filepath, model_filepath = sys.argv[1:]
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
-        X, Y, category_names = load_data(database_filepath)
+        X, Y = load_data(database_filepath)
         X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
         
         print('Building model...')
